3075925652a4c8b3776e81dc8eea40f0fc3560b9
thaisaacastro <thaisascastro@gmail.com>
2020-03-09 20:52:19 -0300

    Conectando a Database

--- node-api/server.js
+++ node-api/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+
+//inicia o app
 const app = express();
+
+
+//Inic o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //req=requisição ao servidor
 //res=resposta para o usuário
+//Rota
 app.get('/', (req, res) => {
   res.send('Olá usuário');
 });
