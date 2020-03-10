8fd4c4a8edbffcfbac54eb90b88704ac676ca233
thaisaacastro <thaisascastro@gmail.com>
2020-03-09 23:36:32 -0300

    Criando o CRUD

--- node-api/src/Controllers/PacienteController.js
+++ node-api/src/Controllers/PacienteController.js
@@ -9,10 +9,26 @@ module.exports = {
     return res.json(pacientes);
 
   },
+
+  async show(req, res) {
+    const paciente = await Paciente.findById(req.params.id);
+
+    return res.json(paciente);
+  },
+
   async store(req, res) {
     const paciente = await Paciente.create(req.body);
 
     return res.json(paciente);
 
   },
+
+  async update(req, res) {
+    const paciente = await Paciente.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    return res.json(paciente);
+  },
+  async destroy(req, res) {
+    await Paciente.findById(req.params.id);
+    return res.send();
+  }
 };
--- node-api/src/routes.js
+++ node-api/src/routes.js
@@ -5,5 +5,8 @@ const PacienteController = require('./Controllers/PacienteController');
 
 //Rota
 routes.get('/pacientes', PacienteController.index);
+routes.get('/pacientes/:id', PacienteController.show);
 routes.post('/pacientes', PacienteController.store);
+routes.put('/pacientes/:id', PacienteController.update);
+routes.delete('/pacientes/:id', PacienteController.destroy);
 module.exports = routes;
